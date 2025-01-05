a87e198f2d2ec00a3d80d896fb2a9817c45c236b
Javlonbek <fayzullaevjavlonbek.d@gmail.com>
2025-01-06 01:00:49 +0500

    setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [
+    HeaderComponent
+  ]
+})
+export class AppComponent {
+  title = 'first-angular-app';
+}
